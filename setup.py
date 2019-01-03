d52829c11572ddfc986b94b607939124d8802d21
Lucas Koerner <koerner.lucas@stthomas.edu>
2019-01-03 15:35:20 -0600

    modify requirements for no version numbers

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='instrbuilder',
-    version="0.0.3",
+    version="0.0.4",
     author='Lucas J. Koerner',
     author_email="koerner.lucas@stthomas.edu",
     description="electrical instrument control",
